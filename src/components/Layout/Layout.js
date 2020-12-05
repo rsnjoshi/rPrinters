ef237c2d920f4ae13a7ede9a05caa8009b916668
prabs15 <sanu.frankboy15@gmail.com>
2020-12-05 21:29:54 +0545

    First commit for project

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import classes from "./Layout.module.css";
+import Aux from "../../hoc/Aux";
+
+class Layout extends Component {
+  render() {
+    return;
+  }
+}
+
+export default Layout;
